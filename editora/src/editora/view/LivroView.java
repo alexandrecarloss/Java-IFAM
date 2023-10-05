8a967f99d844cfd3ac84b4ac15936e1e7ac96a73
Carlos Alexandre <107065086+alexandrecarloss@users.noreply.github.com>
2023-10-04 20:49:45 -0400

    Update LivroView.java

--- editora/src/editora/view/LivroView.java
+++ editora/src/editora/view/LivroView.java
@@ -1,6 +1,5 @@
 package editora.view;
 
-import java.util.List;
 import java.util.Scanner;
 
 import editora.control.LivroController;
@@ -25,7 +24,7 @@ public class LivroView {
 		Livro livro = new Livro();
 		
 		System.out.println("Cadastro de Livro");
-		System.out.print("Informe o título: ");
+		System.out.print("Informe o tÃ­tulo: ");
 		livro.setTitulo(eTitulo.nextLine());
 		do {
 			System.out.print("Informe o autor: ");
@@ -33,8 +32,31 @@ public class LivroView {
 			System.out.print("Deseja informar outro autor (S/N) ?");
 		}while(eNovoAutorSN.next().toLowerCase().equals("s"));
 
-		livroController.inserirLivro(livro);
+		livroController.salvar(livro);
 	}
+	
+	public void atualizarLivro() {
+		Scanner eTitulo = new Scanner(System.in);
+		Scanner eAutor = new Scanner(System.in);
+		Scanner eLivro = new Scanner(System.in);
+		Scanner eNovoAutorSN = new Scanner(System.in);
+		
+		Livro livro = new Livro();
+		System.out.println("AtualizaÃ§Ã£o de Livro");
+		listarLivros();
+		System.out.print("Informe o nÃºmero do livro: ");
+		int indiceLivro = eLivro.nextInt()-1;
+		System.out.print("Informe o tÃ­tulo: ");
+		livro.setTitulo(eTitulo.nextLine());
+		do {
+			System.out.print("Informe o autor: ");
+			livro.inserirAutor(eAutor.nextLine());;
+			System.out.print("Deseja informar outro autor (S/N) ?");
+		}while(eNovoAutorSN.next().toLowerCase().equals("s"));
+
+		livroController.salvar(indiceLivro, livro);
+	}
+	
 	
 	public void listarLivros() {
 		int i = 1;
@@ -48,17 +70,17 @@ public class LivroView {
 		Scanner eNovaPaginaSN = new Scanner(System.in);
 		Scanner eNovaLinhaSN = new Scanner(System.in);
 		
-		System.out.println("Cadastro de páginas");
+		System.out.println("Cadastro de pÃ¡ginas");
 		listarLivros();
-		System.out.print("Informe o número do livro: ");
+		System.out.print("Informe o nÃºmero do livro: ");
 		int indiceLivro = eLivro.nextInt()-1;
 		Livro livro = livroController.obterLivro(indiceLivro);
 		do {
 			Pagina pagina = new Pagina();
-			System.out.printf("Escrevendo a página %d:\n", livro.getPaginas().size()+1);
+			System.out.printf("Escrevendo a pÃ¡gina %d:\n", livro.getPaginas().size()+1);
 			int iLinha=1;
 			do {
-				System.out.printf("Escreva o conteúdo da %da linha:", iLinha++);
+				System.out.printf("Escreva o conteÃºdo da %da linha:", iLinha++);
 				pagina.getLinhas().add(eLinha.nextLine());
 				System.out.print("Deseja escrever outra linha (S/N) ?");
 			}while(eNovaLinhaSN.next().toLowerCase().equals("s"));
@@ -70,16 +92,16 @@ public class LivroView {
 	public void mostrarLivro() {
 		Scanner eLivro = new Scanner(System.in);
 		listarLivros();
-		System.out.print("Informe o número do livro: ");
+		System.out.print("Informe o nÃºmero do livro: ");
 		Livro livro = livroController.obterLivro(eLivro.nextInt()-1);
-		System.out.printf("Título: %s\n", livro.getTitulo());
+		System.out.printf("TÃ­tulo: %s\n", livro.getTitulo());
 		System.out.print("Autores: ");
 		for(String autor:livro.getAutores()) {
 			System.out.printf("%s\n", autor);
 		}
-		System.out.print("Páginas: ");
+		System.out.print("PÃ¡ginas: ");
 		for(Pagina pagina:livro.getPaginas()) {
-			System.out.printf("Número: %d\n", pagina.getNumero());
+			System.out.printf("NÃºmero: %d\n", pagina.getNumero());
 			for(String linha:pagina.getLinhas())
 				System.out.println(linha);
 		}
