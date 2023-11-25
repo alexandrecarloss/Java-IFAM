8b9202d8e43c476f6750a653e7db96b0aa72ef36
Carlos Alexandre <107065086+alexandrecarloss@users.noreply.github.com>
2023-11-24 20:35:47 -0400

    Create Geometria.java

---
+++ Matematica/model/Geometria.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Geometria {
+	public static float CalcularArea(Figura figura) {
+		return figura.area();
+	}
+	
+	public static float CalcularPerimetro(Figura figura) {
+		return figura.perimetro();
+	}
+	
+	public static void Desenhar(Figura figura) {
+		
+	}
+}
